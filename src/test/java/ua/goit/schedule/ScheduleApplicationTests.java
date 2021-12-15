d8057bf1c2f7b70cdef196653af512bcb24d09a8
oleksiikrutko <krutko.av@gmail.com>
2021-12-16 00:38:51 +0200

    first commit. initialization

---
+++ src/test/java/ua/goit/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.goit.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
