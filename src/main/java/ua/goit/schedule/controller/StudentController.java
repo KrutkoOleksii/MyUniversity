42edeb82eb22a78a714b549cc24cbc80e9d779c7
oleksiikrutko <krutko.av@gmail.com>
2021-12-18 01:03:27 +0200

    added StudentController, SubjectController

---
+++ src/main/java/ua/goit/schedule/controller/StudentController.java
@@ -0,0 +1,38 @@
+package ua.goit.schedule.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ua.goit.schedule.model.Student;
+import ua.goit.schedule.service.StudentService;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+
+    @GetMapping(value = {"","/"})
+    public Collection<Student> getAll(){
+        return studentService.findAll();
+    }
+
+    @GetMapping(value = {"/{id}"})
+    public Optional<Student> getById(@PathVariable Long id){
+        return studentService.findById(id);
+    }
+
+    @PostMapping(value = {"/save"})
+    public Student save(@RequestBody Student student){
+        return studentService.save(student);
+    }
+
+    @DeleteMapping(value = {"/{id}"})
+    public void deleteById(@PathVariable Long id) {
+        studentService.deleteById(id);
+    }
+}
---
+++ src/main/java/ua/goit/schedule/controller/SubjectController.java
@@ -0,0 +1,38 @@
+package ua.goit.schedule.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ua.goit.schedule.model.Subject;
+import ua.goit.schedule.service.SubjectService;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/subject")
+public class SubjectController {
+
+    private final SubjectService subjectService;
+
+
+    @GetMapping(value = {"","/"})
+    public Collection<Subject> getAll(){
+        return subjectService.findAll();
+    }
+
+    @GetMapping(value = {"/{id}"})
+    public Optional<Subject> getById(@PathVariable Long id){
+        return subjectService.findById(id);
+    }
+
+    @PostMapping(value = {"/save"})
+    public Subject save(@RequestBody Subject subject){
+        return subjectService.save(subject);
+    }
+
+    @DeleteMapping(value = {"/{id}"})
+    public void deleteById(@PathVariable Long id) {
+        subjectService.deleteById(id);
+    }
+}
